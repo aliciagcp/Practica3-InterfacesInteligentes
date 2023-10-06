df1e77088005f87d69b071756f41b079458e42e5
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-10-06 23:21:55 +0100

    Add files via upload

---
+++ scripts/ejercicio4_script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveUsingKeys : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject cube;
+    public float speed = 0.25f;
+
+    void Update()
+    {
+        // Move the cube using arrow keys (vertical and horizontal axes)
+        float moveVerticalCube = Input.GetAxis("VerticalCube");
+        float moveHorizontalCube = Input.GetAxis("HorizontalCube");
+        Vector3 movementCube = new Vector3(moveHorizontalCube, moveVerticalCube, 0f) * speed;
+        cube.transform.Translate(movementCube);
+
+        // Move the sphere using w-s (vertical movement) and a-d (horizontal movement) keys
+        float moveVerticalSphere = Input.GetAxis("VerticalSphere"); 
+        float moveHorizontalSphere = Input.GetAxis("HorizontalSphere"); 
+        Vector3 movementSphere = new Vector3(moveHorizontalSphere, moveVerticalSphere, 0f) * speed;
+        sphere.transform.Translate(movementSphere);
+    }
+}
