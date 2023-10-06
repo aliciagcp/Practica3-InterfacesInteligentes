9399ff5c419243472dc6a450077696cc5a5692e5
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-10-06 18:39:55 +0100

    Add files via upload

--- scripts/ejercicio3_script.cs
+++ scripts/ejercicio3_script.cs
@@ -6,10 +6,11 @@ public class Ejercicio3_3 : MonoBehaviour
 {
     public Vector3 moveDirection = new Vector3(1f, 0f, 0f);
     public float speed = 2f;
+    public Space referenceSpace = Space.Self;
 
     void Update()
     {
         Vector3 displacement = moveDirection * speed * Time.deltaTime;
-        transform.Translate(displacement);
+        transform.Translate(displacement,  referenceSpace);
     }
 }
