4c3d9c16fec126a80ae46e2174722f50e1901f34
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-10-08 19:18:12 +0100

    Update ejercicio5_script.cs

--- scripts/ejercicio5_script.cs
+++ scripts/ejercicio5_script.cs
@@ -13,13 +13,13 @@ public class moveUsingKeys : MonoBehaviour
         // Move the cube using arrow keys (vertical and horizontal axes)
         float moveVerticalCube = Input.GetAxis("VerticalCube");
         float moveHorizontalCube = Input.GetAxis("HorizontalCube");
-        Vector3 movementCube = new Vector3(moveHorizontalCube, moveVerticalCube, 0f) * speed * Time.deltaTime;
+        Vector3 movementCube = new Vector3(moveHorizontalCube, 0f, moveVerticalCube) * speed * Time.deltaTime;
         cube.transform.Translate(movementCube);
 
         // Move the sphere using w-s (vertical movement) and a-d (horizontal movement) keys
         float moveVerticalSphere = Input.GetAxis("VerticalSphere"); 
         float moveHorizontalSphere = Input.GetAxis("HorizontalSphere"); 
-        Vector3 movementSphere = new Vector3(moveHorizontalSphere, moveVerticalSphere, 0f) * speed * Time.deltaTime;
+        Vector3 movementSphere = new Vector3(moveHorizontalSphere, 0f, moveVerticalSphere) * speed * Time.deltaTime;
         sphere.transform.Translate(movementSphere);
     }
 }
