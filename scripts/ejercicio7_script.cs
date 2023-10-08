7f823296d0d80178dedd397664a71fe90989c33e
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-10-08 20:29:03 +0100

    Add files via upload

---
+++ scripts/ejercicio7_script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveUsingKeys : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject cube;
+    public float speed = 5f;
+
+    void Update()
+    {
+        Vector3 directionToSphere = sphere.transform.position - cube.transform.position;
+        cube.transform.LookAt(sphere.transform, Vector3.up);
+        Vector3 movement = cube.transform.forward * speed * Time.deltaTime;
+        cube.transform.Translate(movement, Space.World);
+
+        // Mueve la esfera usando w-s (movimiento vertical) y a-d (movimiento horizontal) keys
+        float moveVerticalSphere = Input.GetAxis("VerticalSphere"); 
+        float moveHorizontalSphere = Input.GetAxis("HorizontalSphere"); 
+        Vector3 movementSphere = new Vector3(moveHorizontalSphere, 0f, moveVerticalSphere) * speed * Time.deltaTime;
+        sphere.transform.Translate(movementSphere);
+    }
+}
